5de08f0f9b1c0021477f7a9c9501752e6f045d6b
poppy1006 <poppyinfo0@gmail.com>
2024-08-19 09:13:24 +0530

    feat: :sparkles: init

--- medusa-config.js
+++ medusa-config.js
@@ -29,7 +29,12 @@ const ADMIN_CORS =
 const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
 
 const DATABASE_URL =
-  process.env.DATABASE_URL || "postgres://localhost/medusa-starter-default";
+  // process.env.DATABASE_URL || "postgresql://monorail.proxy.rlwy.net:25977/railway";
+
+  process.env.DATABASE_URL || "postgresql://postgres:CtyRxRboNIVigzwkRRewjacSglHiXAjA@monorail.proxy.rlwy.net:25977/railway";
+
+
+  // postgresql://monorail.proxy.rlwy.net:25977/railway
 
 const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
 
